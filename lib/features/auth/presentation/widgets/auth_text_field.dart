1920005e573f25076e1ac246d9da5b4400b909da
AmrelsayeD222 <amrptmode@gmail.com>
2026-04-17 16:59:59 +0200

    feat: add reusable AuthTextField widget with password visibility toggle support

--- lib/features/auth/presentation/widgets/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/auth_text_field.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:k_rehab/core/theme/app_colors.dart';
 import 'package:k_rehab/core/theme/app_text_styles.dart';
 
-class AuthTextField extends StatelessWidget {
+class AuthTextField extends StatefulWidget {
   final TextEditingController controller;
   final String hintText;
   final bool isPassword;
   final TextInputType keyboardType;
   final String? Function(String?)? validator;
+  final Widget? suffixIcon;
 
   const AuthTextField({
     super.key,
@@ -16,20 +17,51 @@ class AuthTextField extends StatelessWidget {
     this.isPassword = false,
     this.keyboardType = TextInputType.text,
     this.validator,
+    this.suffixIcon,
   });
+
+  @override
+  State<AuthTextField> createState() => _AuthTextFieldState();
+}
+
+class _AuthTextFieldState extends State<AuthTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       cursorColor: AppColors.primary,
       onTapOutside: (event) => FocusScope.of(context).unfocus(),
-      controller: controller,
-      obscureText: isPassword,
-      keyboardType: keyboardType,
-      validator: validator,
+      controller: widget.controller,
+      obscureText: _obscureText,
+      keyboardType: widget.keyboardType,
+      validator: widget.validator,
       style: AppTextStyles.bodyText1,
       decoration: InputDecoration(
-        hintText: hintText,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off_rounded : Icons.visibility_rounded,
+                  color: _obscureText ? AppColors.textMuted : AppColors.primary,
+                  size: 22,
+                ),
+                onPressed: _toggleVisibility,
+                splashRadius: 24,
+              )
+            : widget.suffixIcon,
+        hintText: widget.hintText,
         hintStyle: AppTextStyles.bodyText2.copyWith(color: AppColors.textMuted),
         filled: true,
         fillColor: AppColors.cardBackground,
